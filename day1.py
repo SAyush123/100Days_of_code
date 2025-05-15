2194a6f8701ea4e0a774dfb8ec0478aff7cc46f8
SAyush123 <ayushsraj123@gmail.com>
2025-05-15 13:46:33 +0530

    Band Name Generator Completed!

--- day1.py
+++ day1.py
@@ -1,4 +1,4 @@
 print("Welcome to the Band Name Generator!")
-city_name = input("What city did you grow up in? ")
-pet_name = input("What is the name of your first pet? ")
-print(f"Your band name is {city_name} {pet_name}")
+city_name = input("What city did you grow up in? ").strip()
+pet_name = input("What is the name of your first pet? ").strip()
+print(f"Your band name is {city_name.title()} {pet_name.title()}")
